934e438354aa8c98b2020dd1b04b1ef9a4fbf6c4
Abdullah Sufyan Murad <abdullahmurad2002@gmail.com>
2024-10-22 08:49:56 +0400

    Build Pipeline Workflow

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,25 @@
-// App.js will contain the express code, instead of server.js
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require("./routes/planets/plantes.router");
+const launchesRouter = require('./routes/launches/launches.router');
 
-const app = express(); // Set up express
+const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+  origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-
-app.use(express.json());  // The built in json middleware parses any incoming JSON from 
-                        // the body of incoming requests
-app.use(express.static(path.join(__dirname, "..", "public")));
-                        
-app.use(planetsRouter);
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-})
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require("mongoose");
 
 const app = require('./app');
 
@@ -6,9 +7,22 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://nasa-api:IFwxrtgNs1F9T2oQ@nasacluster.m7bpw.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  
+  await mongoose.connect(MONGO_URL);
+
   await loadPlanetsData();
   
   server.listen(PORT, () => {
