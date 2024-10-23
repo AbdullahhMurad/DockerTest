26af73c4354ca559ab53ab42b547b527378a1fd4
Abdullah Sufyan Murad <abdullahmurad2002@gmail.com>
2024-10-23 08:31:33 +0400

    First Commit

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,65 @@
 FROM node:lts-alpine
 
+# Set the working directory
 WORKDIR /app
 
-COPY package*.json ./ 
+# Copy the root package files first (if they exist)
+COPY package*.json ./
 
+# Install dependencies for the client and log errors if they occur
+WORKDIR /app/client
+COPY client/package*.json ./
+RUN npm install --omit=dev || cat /root/.npm/_logs/*debug*.log
 
-COPY client/package*.json client/
-RUN npm install-client --omit=dev
+# Install dependencies for the server and log errors if they occur
+WORKDIR /app/server
+COPY server/package*.json ./
+RUN npm install --omit=dev || cat /root/.npm/_logs/*debug*.log
 
-COPY server/package*.json server/
-RUN npm run install-server --omit=dev
+# Build the client and log errors if they occur
+WORKDIR /app/client
+COPY client/ ./
+RUN npm run build || cat /root/.npm/_logs/*debug*.log
 
-COPY client/ client/
-RUN npm run build --prefix client
+# Copy the server files
+WORKDIR /app/server
+COPY server/ ./
 
-COPY server/ server/
-
+# Use a non-root user
 USER node
 
-CMD [ "npm", "start", "--prefix", "server" ]
+# Set the command to start the server
+CMD [ "npm", "start" ]
 
+# Expose port 8000
 EXPOSE 8000
+
+
+
+
+
+
+
+# FROM node:lts-alpine
+
+# WORKDIR /app
+
+# COPY package*.json ./ 
+
+
+# COPY client/package*.json client/
+# RUN npm install-client --omit=dev
+
+# COPY server/package*.json server/
+# RUN npm run install-server --omit=dev
+
+# COPY client/ client/
+# RUN npm run build --prefix client
+
+# COPY server/ server/
+
+# USER node
+
+# CMD [ "npm", "start", "--prefix", "server" ]
+
+# EXPOSE 8000
--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,35 @@
 const http = require('http');
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+const express = require("express");
 
 const app = require('./app');
-
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = "mongodb+srv://nasa-api:IFwxrtgNs1F9T2oQ@nasacluster.m7bpw.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
+// const MONGO_URL = "mongodb+srv://nasa-api:IFwxrtgNs1F9T2oQ@nasacluster.m7bpw.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
+// mongoose.connection.once("open", () => {
+//   console.log("MongoDB connection ready!");
+// });
 
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
+// mongoose.connection.on("error", (err) => {
+//   console.error(err);
+// });
 
 async function startServer() {
-  
-  await mongoose.connect(MONGO_URL);
 
+    await mongoConnect();
+
+  //  Instead of 
+  // await mongoose.connect(MONGO_URL);
+ 
+  // The four arguments that were added have been deprecated
+  
   await loadPlanetsData();
   
   server.listen(PORT, () => {
@@ -31,3 +38,41 @@ async function startServer() {
 }
 
 startServer();
+
+
+
+
+
+// const http = require('http');
+// const mongoose = require("mongoose");
+
+// const app = require('./app');
+
+// const { loadPlanetsData } = require('./models/planets.model');
+
+// const PORT = process.env.PORT || 8000;
+
+// const MONGO_URL = "mongodb+srv://nasa-api:IFwxrtgNs1F9T2oQ@nasacluster.m7bpw.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
+
+// const server = http.createServer(app);
+
+// mongoose.connection.once("open", () => {
+//   console.log("MongoDB connection ready!");
+// });
+
+// mongoose.connection.on("error", (err) => {
+//   console.error(err);
+// });
+
+// async function startServer() {
+  
+//   await mongoose.connect(MONGO_URL);
+
+//   await loadPlanetsData();
+  
+//   server.listen(PORT, () => {
+//     console.log(`Listening on port ${PORT}...`);
+//   });
+// }
+
+// startServer();
